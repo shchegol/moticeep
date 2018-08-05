f7af59f3513dcaf081b46ffd06becfb90939379f
Alexander Shchegol <zelenzoom@gmail.com>
2018-08-05 14:48:10 +0300

    - bootstrap added; - start auth system

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,70 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        motikeep-nuxt
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div class="container">
+    <header>
+      <div class="row align-items-center">
+        <div class="col">
+          <h1>MOTIKEEP</h1>
+        </div>
+
+        <div class="col">
+          <button class="btn btn-primary">
+            Войти
+          </button>
+
+          <button class="btn btn-primary">
+            Зарегестироваться
+          </button>
+        </div>
       </div>
-    </div>
-  </section>
+    </header>
+
+    <section class="row mt-4">
+      <div class="col">
+        private: {{ isLogin }}
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+  import AppLogo from '~/components/AppLogo.vue';
 
-export default {
-  components: {
-    AppLogo
-  }
-}
+  export default {
+    components: {
+      AppLogo,
+    },
+
+    data() {
+      return {
+        isLogin: false,
+      };
+    },
+
+    methods: {
+      login() {
+        axios
+          .post('/login')
+          .then(res => {
+            console.log(res.data)
+            this.isLogin = true;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      },
+
+      register() {
+        axios
+          .post('/register')
+          .then(res => {
+            console.log(res.data)
+            this.isLogin = true;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      },
+    },
+  };
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
 
