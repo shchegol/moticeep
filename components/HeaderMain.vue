72f4b944e8a90077933e6fb4c698f18ff0edcba7
Alexander Shchegol <zelenzoom@gmail.com>
2018-08-16 12:22:09 +0300

    main header /    - main header component created;

---
+++ components/HeaderMain.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="row align-items-center">
+    <div class="col">
+      <h1>MOTIKEEP</h1>
+    </div>
+
+    <div class="col-auto">
+      <nuxt-link v-if="!auth" to="/auth" class="btn btn-link">Войти</nuxt-link>
+      <button v-else @click="logout" class="btn btn-link">Выйти</button>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'header-main',
+
+    props: {
+      auth: {
+        type: Boolean,
+        required: true
+      }
+    },
+
+    methods: {
+      async logout() {
+        try {
+          await this.$store.dispatch('logout');
+        } catch (e) {
+          console.log(e.message);
+        }
+      },
+    },
+  };
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,17 @@
 <template>
   <div class="container">
-    <header class="row align-items-center">
-      <div class="col">
-        <h1>MOTIKEEP</h1>
-      </div>
-
-      <div class="col-auto">
-        <nuxt-link v-if="!auth" to="/auth" class="btn btn-link">Войти</nuxt-link>
-        <button v-else @click="logout" class="btn btn-link">Выйти</button>
-      </div>
-    </header>
-
-
+    <main-header :auth="Boolean(auth)"></main-header>
   </div>
 </template>
 
 <script>
   import {mapState} from 'vuex';
+  import Header     from '~/components/HeaderMain';
 
   export default {
-    components: {},
+    components: {
+      'main-header': Header,
+    },
 
     data() {
       return {};
@@ -28,16 +20,6 @@
     computed: mapState([
       'auth',
     ]),
-
-    methods: {
-      async logout() {
-        try {
-          await this.$store.dispatch('logout');
-        } catch (e) {
-          console.log(e.message);
-        }
-      },
-    },
   };
 </script>
 
