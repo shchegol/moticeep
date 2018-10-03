9554669ec46966cd237f26e16285f5afb14d6621
Alexander Shchegol <zelenzoom@gmail.com>
2018-10-03 23:08:55 +0300

    Main layout:    - action button customized    - modal window for task and motivators added

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,139 @@
     <v-divider class="mt-5 mb-5"></v-divider>
     <motivators></motivators>
 
-    <v-btn
-      fab
-      color="accent"
+    <v-speed-dial
+      v-model="fab"
       fixed
       bottom
       right
-      @click="dialog = !dialog"
+      direction="top"
     >
-      <v-icon>add</v-icon>
-    </v-btn>
+
+      <v-btn
+        slot="activator"
+        v-model="fab"
+        color="accent"
+        dark
+        fab
+        class="ma-2"
+      >
+        <v-icon>add</v-icon>
+        <v-icon>close</v-icon>
+      </v-btn>
+
+      <v-btn
+        fab
+        dark
+        small
+        color="second"
+        @click="dialogTask = true"
+      >
+        <v-icon>assignment</v-icon>
+        <span class="v-btn__label">Задание</span>
+      </v-btn>
+
+      <v-btn
+        fab
+        dark
+        small
+        color="second"
+        @click="dialogMotivator = true"
+      >
+        <v-icon>cake</v-icon>
+        <span class="v-btn__label">Мотиватор</span>
+      </v-btn>
+    </v-speed-dial>
+
+
+
+    <v-dialog
+      v-model="dialogTask"
+      width="500"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="headline">Создать задание</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container class="pa-0">
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-text-field
+                  label="Заголовок"
+                  placeholder="За труды"
+                  box
+                  class="w-100"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Отменить
+          </v-btn>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Создать
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
+    <v-dialog
+      v-model="dialogMotivator"
+      width="500"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="headline">Создать мотиватор</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container class="pa-0">
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-text-field
+                  label="Заголовок"
+                  placeholder="Мечта"
+                  box
+                  class="w-100"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Отменить
+          </v-btn>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Создать
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -29,7 +152,9 @@
     middleware: 'notAuthenticated',
     data() {
       return {
-        dialog: false,
+        dialogTask: false,
+        dialogMotivator: false,
+        fab: false,
       };
     },
   };
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -3,9 +3,9 @@ import Vuetify from 'vuetify';
 
 Vue.use(Vuetify, {
   theme: {
-    primary: "#4CAF50",
-    secondary: "#66BB6A",
-    accent: "#FF5722",
+    primary: "#9C27B0",
+    secondary: "#AB47BC",
+    accent: "#E91E63",
     error: "#f44336",
     warning: "#ffeb3b",
     info: "#2196f3",
