87fc53b211e51dbd8ed14a46367807adbe4a90f5
Alexander Shchegol <zelenzoom@gmail.com>
2018-10-25 11:17:38 +0300

    - minor changes in index.js

--- server/index.js
+++ server/index.js
@@ -11,17 +11,20 @@ async function start() {
         port = process.env.PORT || config.server.port,
         app  = new Koa();
 
+  let serverInfo = `
+  ########## success ########## \x1b[32m
+  environment: ${process.env.NODE_ENV}
+  host:        ${host}
+  port:        ${port}\x1b[0m
+  #############################
+  `;
+
   app.proxy = true;
   app.keys = [config.secret];
 
   // nuxt.js
   nuxtConfig.dev = !(app.env === 'production');
   const nuxt = new Nuxt(nuxtConfig);
-
-  console.log('nuxtConfig.dev', nuxtConfig.dev);
-  console.log('app.env', app.env);
-  console.log('process.env.HOST', process.env.HOST);
-  console.log('process.env.PORT', process.env.PORT);
 
   if (nuxtConfig.dev) {
     const builder = new Builder(nuxt);
@@ -38,8 +41,6 @@ async function start() {
   app.use(async (ctx, next) => {
     ctx.status = 200;
 
-    // console.log('session maxAge', ctx.session)
-
     ctx.req.session = ctx.session;
     ctx.req.state = ctx.state;
     await koaConnect(nuxt.render)(ctx, next);
@@ -47,7 +48,7 @@ async function start() {
 
   app.listen(port, host);
 
-  console.log(`Server listening on ${host}:${port}`); // eslint-disable-line no-console
+  console.log(serverInfo);
 }
 
 start();
